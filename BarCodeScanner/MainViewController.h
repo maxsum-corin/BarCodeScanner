5466006568a003861e9b0554f7d88a8b4972610f
Corin Lawson <corin@phiware.com.au>
2011-10-18 13:49:21 +1100

    Initial Commit

---
+++ BarCodeScanner/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  BarCodeScanner
+//
+//  Created by Corin Lawson on 18/10/11.
+//  Copyright (c) 2011 Corin Lawson. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@end
---
+++ BarCodeScanner/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BarCodeScanner
+//
+//  Created by Corin Lawson on 18/10/11.
+//  Copyright (c) 2011 Corin Lawson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
